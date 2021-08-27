cc3fc4d41d48568cbade65629c81e4c4f156d1eb
anamika.sawhney22@gmail.com <anamika.sawhney22@gmail.com>
2021-08-27 06:19:22 +0000

    Code First in MVC

---
+++ CodeFirstApproachDemo/Models/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachDemo.Models
+{
+    public class EmployeeDbContext: DbContext // FOr this we need Microsoft.EntityFrameworkCore;
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+                   : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
